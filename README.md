ffb5a41215a2ce889f82b2abafd9d6af508931c9
Val Mandantes <mandantesv@gmail.com>
2018-01-03 22:40:21 +0800

    v9.3

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
-## Terminal App Systemizer
-Systemize your App systemlessly!
-Using terminal emulator.
-Enter this command and choose the app you want to systemize.
+# Magisk Module Template
 
-	systemize
-	
-And Reboot to apply changes.
+This `README.md` will be shown in Magisk Manager. Place any information / changelog / notes you like.
+
+**Please update `README.md` if you want to submit your module to the online repo!**
+
+Github has its own online markdown editor with a preview feature, you can use it to update your `README.md`! If you need more advanced syntax, check the [Markdown Cheat Sheet](https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet).
+
+For more information about modules and repos, please check the [official documentations](https://github.com/topjohnwu/Magisk/blob/master/docs/module_repo.md)
--- config.sh
+++ config.sh
@@ -1,33 +1,24 @@
 ##########################################################################################
 #
-# Magisk
+# Magisk Module Template Config Script
 # by topjohnwu
-# 
-# This is a template zip for developers
 #
 ##########################################################################################
 ##########################################################################################
-# 
+#
 # Instructions:
-# 
+#
 # 1. Place your files into system folder (delete the placeholder file)
 # 2. Fill in your module's info into module.prop
-# 3. Configure the settings in this file (common/config.sh)
-# 4. For advanced features, add shell commands into the script files under common:
-#    post-fs-data.sh, service.sh
-# 5. For changing props, add your additional/modified props into common/system.prop
-# 
+# 3. Configure the settings in this file (config.sh)
+# 4. If you need boot scripts, add them into common/post-fs-data.sh or common/service.sh
+# 5. Add your additional or modified system properties into common/system.prop
+#
 ##########################################################################################
 
 ##########################################################################################
-# Defines
+# Configs
 ##########################################################################################
-
-# NOTE: This part has to be adjusted to fit your own needs
-
-# This will be the folder name under /magisk
-# This should also be the same as the id in your module.prop to prevent confusion
-MODID=terminal_systemizer
 
 # Set to true if you need to enable Magic Mount
 # Most mods would like it to be enabled
@@ -49,10 +40,9 @@ LATESTARTSERVICE=false
 # Set what you want to show when installing your mod
 
 print_modname() {
-  ui_print "******************************"
-  ui_print "   Terminal App Systemizer    "
-  ui_print "  by veez21 @ xda-developers  "
-  ui_print "******************************"
+  ui_print "*******************************"
+  ui_print "     Magisk Module Template    "
+  ui_print "*******************************"
 }
 
 ##########################################################################################
@@ -60,10 +50,8 @@ print_modname() {
 ##########################################################################################
 
 # List all directories you want to directly replace in the system
-# By default Magisk will merge your files with the original system
-# Directories listed here however, will be directly mounted to the correspond directory in the system
+# Check the documentations for more info about how Magic Mount works, and why you need this
 
-# You don't need to remove the example below, these values will be overwritten by your own list
 # This is an example
 REPLACE="
 /system/app/Youtube
@@ -72,7 +60,7 @@ REPLACE="
 /system/framework
 "
 
-# Construct your own list here, it will overwrite the example
+# Construct your own list here, it will override the example above
 # !DO NOT! remove this if you don't need to replace anything, leave it empty as it is now
 REPLACE="
 "
@@ -81,16 +69,11 @@ REPLACE="
 # Permissions
 ##########################################################################################
 
-# NOTE: This part has to be adjusted to fit your own needs
-
 set_permissions() {
-  # Default permissions, don't remove them
-  set_perm_recursive  $MODPATH  0  0  0755  0644
-
   # Only some special files require specific permissions
   # The default permissions should be good enough for most cases
 
-  # Some templates if you have no idea what to do:
+  # Here are some examples for the set_perm functions:
 
   # set_perm_recursive  <dirname>                <owner> <group> <dirpermission> <filepermission> <contexts> (default: u:object_r:system_file:s0)
   # set_perm_recursive  $MODPATH/system/lib       0       0       0755            0644
@@ -99,20 +82,35 @@ set_permissions() {
   # set_perm  $MODPATH/system/bin/app_process32   0       2000    0755         u:object_r:zygote_exec:s0
   # set_perm  $MODPATH/system/bin/dex2oat         0       2000    0755         u:object_r:dex2oat_exec:s0
   # set_perm  $MODPATH/system/lib/libart.so       0       0       0644
+
+  # The following is default permissions, DO NOT remove
+  set_perm_recursive  $MODPATH  0  0  0755  0644
   cp -af $INSTALLER/common/aapt $MODPATH/aapt
   set_perm $MODPATH/system/bin/systemize 0 0 0777
   set_perm $MODPATH/aapt 0 0 0777
 }
 
+##########################################################################################
+# Custom Functions
+##########################################################################################
+
+# This file (config.sh) will be sourced by the main flash script after util_functions.sh
+# If you need custom logic, please add them here as functions, and call these functions in
+# update-binary. Refrain from adding code directly into update-binary, as it will make it
+# difficult for you to migrate your modules to newer template versions.
+# Make update-binary as clean as possible, try to only do function calls in it.
+
 detect_installed() {
   no_app=0
   no_privapp=0
+  no_xml=0
   all_apk_size=0
   mkdir -p $TMPDIR/$MODID/system
   mkdir -p $TMPDIR/$MODID/system/app
   mkdir -p $TMPDIR/$MODID/system/priv-app
-  if [ -d /magisk/$MODID/system/app ]; then
-    for i in $(find /magisk/$MODID/system/app -name *.apk -type f); do
+  mkdir -p $TMPDIR/$MODID/system/etc/permissions
+  if [ -d $MODPATH/system/app ]; then
+    for i in $(find $MODPATH/system/app -name *.apk -type f); do
 	  if [ "$1" != "size" ]; then
         cp -af ${i%/*} $TMPDIR/$MODID/system/app
 	  fi
@@ -121,8 +119,8 @@ detect_installed() {
   else
     no_app=1
   fi
-  if [ -d /magisk/$MODID/system/priv-app ]; then
-    for i in $(find /magisk/$MODID/system/priv-app -name *.apk -type f); do
+  if [ -d $MODPATH/system/priv-app ]; then
+    for i in $(find $MODPATH/system/priv-app -name *.apk -type f); do
 	  if [ "$1" != "size" ]; then
         cp -af ${i%/*} $TMPDIR/$MODID/system/priv-app
 	  fi
@@ -131,21 +129,35 @@ detect_installed() {
   else
     no_privapp=1
   fi
+  if [ -d $MODPATH/system/etc/permissions ]; then
+    for i in $(find $MODPATH/system/etc/permissions -name *.xml -type f); do
+	  if [ "$1" != "size" ]; then
+	    cp -af ${i%/*} $TMPDIR/$MODID/system/etc/permissions
+      fi
+	done
+  else
+    no_xml=1
+  fi
+  #reqSizeM=$((reqSizeM+all_apk_size))
 }
 
 reinstall() {
   if [ $no_app == 0 ]; then
     for i in $(find $TMPDIR/$MODID/system/app -name *.apk -type f); do
 	  app=${i%/*}
-      mkdir -p /magisk/$MODID/system/app/${app##*/}
-      cp -af $app/. /magisk/$MODID/system/app/${app##*/}
+      mkdir -p $MODPATH/system/app/${app##*/}
+      cp -af $app/. $MODPATH/system/app/${app##*/}
     done
   fi
   if [ $no_privapp == 0 ]; then
     for i in $(find $TMPDIR/$MODID/system/priv-app -name *.apk -type f); do
 	  app=${i%/*}
-      mkdir -p /magisk/$MODID/system/priv-app/${app##*/}
-      cp -af $app/. /magisk/$MODID/system/priv-app/${app##*/}
+      mkdir -p $MODPATH/system/priv-app/${app##*/}
+      cp -af $app/. $MODPATH/system/priv-app/${app##*/}
     done
+  fi
+  if [ $no_xml == 0 ]; then
+	mkdir -p $MODPATH/system/etc/permissions
+	cp -af $TMPDIR/$MODID/system/etc/permissions/* $MODPATH/system/etc/permissions
   fi
 }
