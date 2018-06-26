133933885b705a53317ea8370cf383cca9408e25
Val Mandantes <mandantesv@gmail.com>
2018-06-26 22:47:20 +0800

    y dis hapen ;_;
    Signed-off-by: Val Mandantes <mandantesv@gmail.com>

--- config.sh
+++ config.sh
@@ -87,7 +87,7 @@ set_permissions() {
   # The following is default permissions, DO NOT remove
   set_perm_recursive  $MODPATH  0  0  0755  0644
   cp -af $INSTALLER/common/aapt $MODPATH/aapt
-  cp -af $INSTALLER/common/mod-util/mod-util.sh $MODPATH/mod-util.sh
+  cp -af $INSTALLER/common/mod-util.sh $MODPATH/mod-util.sh
   bin=bin
   if (grep -q samsung /system/build.prop); then
     bin=xbin
