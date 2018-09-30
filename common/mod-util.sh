491467e04c4536ce3eabf50f5bb4031fae6d6111
Val Mandantes <mandantesv@gmail.com>
2018-09-30 17:47:53 +0800

    Update mod-util.sh
    Signed-off-by: Val Mandantes <mandantesv@gmail.com>

--- common/mod-util.sh
+++ common/mod-util.sh
@@ -1,57 +1,57 @@
 ##########################################################################################
 #
-# veez21 Terminal Utility Functions
+# Terminal Utility Functions
 # by veez21
 #
 ##########################################################################################
 
-MODUTILVER=v1
-MODUTILVCODE=1
+MODUTILVER=v2
+MODUTILVCODE=2
 
 #=========================== Determine if A/B OTA device
 if [ -d /system_root ]; then
-	isABDevice=true
-	SYSTEM=/system_root/system
-	SYSTEM2=/system
+  isABDevice=true
+  SYSTEM=/system_root/system
+  SYSTEM2=/system
 else
-	isABDevice=false
-	SYSTEM=/system
-	SYSTEM2=/system
+  isABDevice=false
+  SYSTEM=/system
+  SYSTEM2=/system
 fi
 
 #=========================== Set Busybox (Used by Magisk) up
 set_busybox() {
-	if [ -x "$1" ]; then
-		for i in $(${1} --list); do
-			if [ "$i" != 'echo' ]; then
-				alias "$i"="${1} $i" 2>>$LOG >>$LOG
-			fi
-		done
-		_busybox=true
-		_bb=$1
-	fi
+  if [ -x "$1" ]; then
+    for i in $(${1} --list); do
+      if [ "$i" != 'echo' ]; then
+        alias "$i"="${1} $i" 2>>$LOG >>$LOG
+      fi
+    done
+    _busybox=true
+    _bb=$1
+  fi
 }
 _busybox=false
 if [ -d /sbin/.core/busybox ]; then
-	PATH=$PATH:/sbin/.core/busybox
-	_bb=/sbin/.core/busybox/busybox
-	_busybox=true
+  PATH=$PATH:/sbin/.core/busybox
+  _bb=/sbin/.core/busybox/busybox
+  _busybox=true
 elif [ ! -x $SYSTEM/xbin/busybox ]; then
-	set_busybox /data/magisk/busybox
-	set_busybox /data/adb/magisk/busybox
+  set_busybox /data/magisk/busybox
+  set_busybox /data/adb/magisk/busybox
 else
-	alias busybox=""
+  alias busybox=""
 fi
 if [ -x $SYSTEM/xbin/busybox ]; then
-	_bb=$SYSTEM/xbin/busybox
+  _bb=$SYSTEM/xbin/busybox
 elif [ -x $SYSTEM/bin/busybox ]; then
-	_bb=$SYSTEM/bin/busybox
+  _bb=$SYSTEM/bin/busybox
 elif [ $_busybox ]; then
-	true
+  true
 else
-	echo "! Busybox not detected.."
-	echo "Please install one (@osm0sis' busybox recommended)"
-	false
+  echo "! Busybox not detected.."
+  echo "Please install one (@osm0sis' busybox recommended)"
+  false
 fi
 [ $? -ne 0 ] && exit $?
 alias echo='echo -e'
@@ -60,8 +60,8 @@ _bbname=$(busybox | head -n1)
 _bbname=${_bbname%'('*}
 BBok=true
 if [ "$_bbname" == "" ]; then
-	_bbname="BusyBox not found!"
-	BBok=false
+  _bbname="BusyBox not found!"
+  BBok=false
 fi
 
 #=========================== Default Functions and Variables
@@ -101,42 +101,42 @@ loadBar=' '			# Load UI
 # Remove color codes if -nc or in ADB Shell
 [ -n "$1" -a "$1" == "-nc" ] && shift && NC=true
 [ "$NC" -o -n "$LOGNAME" ] && {
-	G=''; R=''; Y=''; B=''; V=''; Bl=''; C=''; W=''; N=''; BGBL=''; loadBar='=';
+  G=''; R=''; Y=''; B=''; V=''; Bl=''; C=''; W=''; N=''; BGBL=''; loadBar='=';
 }
 
 # Divider (based on $MODTITLE, $VER, and $REL characters)
-character_no=$(echo $MODTITLE $VER $REL | tr " " '_' | wc -c)
+character_no=$(echo "$MODTITLE $VER $REL" | tr " " '_' | wc -c)
 div="${Bl}$(printf '%*s' "${character_no}" '' | tr " " "=")${N}"
 
 # Title Div
 title_div() {
-	no=$(echo $@ | wc -c)
-	extdiv=$((no-character_no))
-	echo "${W}$@${N} ${Bl}$(printf '%*s' "$extdiv" '' | tr " " "=")${N}"
+  no=$(echo "$@" | wc -c)
+  extdiv=$((no-character_no))
+  echo "${W}$@${N} ${Bl}$(printf '%*s' "$extdiv" '' | tr " " "=")${N}"
 }
 
 # set_file_prop <property> <value> <prop.file>
 set_file_prop() {
-	if [ -f "$3" ]; then
-		if grep "$1=" $3; then
-			sed -i "s/${1}=.*/${1}=${2}/g" $3
-		else
-			echo "$1=$2" >> $3
-		fi
-	else
-		echo "$3 doesn't exist!"
-	fi
+  if [ -f "$3" ]; then
+    if grep "$1=" "$3"; then
+      sed -i "s/${1}=.*/${1}=${2}/g" "$3"
+    else
+      echo "$1=$2" >> "$3"
+    fi
+  else
+    echo "$3 doesn't exist!"
+  fi
 }
 
 # https://github.com/fearside/ProgressBar
 ProgressBar() {
 # Process data
-	_progress=$(((${1}*100/${2}*100)/100))
-	_done=$(((${_progress}*4)/10))
-	_left=$((40-$_done))
+  _progress=$(((${1}*100/${2}*100)/100))
+  _done=$(((${_progress}*4)/10))
+  _left=$((40-$_done))
 # Build progressbar string lengths
-	_done=$(printf "%${_done}s")
-	_left=$(printf "%${_left}s")
+  _done=$(printf "%${_done}s")
+  _left=$(printf "%${_left}s")
 
 # 1.2 Build progressbar strings and print the ProgressBar line
 # 1.2.1 Output example:
@@ -149,12 +149,12 @@ Spinner() {
 
 # Choose which character to show.
 case ${_indicator} in
-	"|") _indicator="/";;
-	"/") _indicator="-";;
-	"-") _indicator="\\";;
-	"\\") _indicator="|";;
-	# Initiate spinner character
-	*) _indicator="\\";;
+  "|") _indicator="/";;
+  "/") _indicator="-";;
+  "-") _indicator="\\";;
+  "\\") _indicator="|";;
+  # Initiate spinner character
+  *) _indicator="\\";;
 esac
 
 # Print simple progress spinner
@@ -163,52 +163,52 @@ printf "\r${@} [${_indicator}]"
 
 # "cmd & spinner [message]"
 e_spinner() {
-	PID=$!
-	h=0; anim='-\|/';
-	while [ -d /proc/$PID ]; do
-		h=$(((h+1)%4))
-		sleep 0.02
-		printf "\r${@} [${anim:$h:1}]"
-	done
+  PID=$!
+  h=0; anim='-\|/';
+  while [ -d /proc/$PID ]; do
+    h=$(((h+1)%4))
+    sleep 0.02
+    printf "\r${@} [${anim:$h:1}]"
+  done
 }
 
 # test_connection
 test_connection() {
-	echo -n "Testing internet connection "
-	ping -q -c 1 -W 1 google.com >/dev/null 2>/dev/null && echo "- OK" || { echo "Error"; false; }
+  echo -n "Testing internet connection "
+  ping -q -c 1 -W 1 google.com >/dev/null 2>/dev/null && echo "- OK" || { echo "Error"; false; }
 }
 
 # Log files will be uploaded to termbin.com
 upload_logs() {
-	$BBok && {
-		test_connection
-		[ $? -ne 0 ] && exit
-		verUp=none; oldverUp=none; logUp=none; oldlogUp=none;
-		echo "Uploading logs"
-		[ -s $VERLOG ] && verUp=$(cat $VERLOG | nc termbin.com 9999)
-		[ -s $oldVERLOG ] && oldverUp=$(cat $oldVERLOG | nc termbin.com 9999)
-		[ -s $LOG ] && logUp=$(cat $LOG | nc termbin.com 9999)
-		[ -s $oldLOG ] && oldlogUp=$(cat $oldLOG | nc termbin.com 9999)
-		echo -n "Link: "
-		echo "$MODEL ($DEVICE) API $API\n$ROM\n$ID\n
-		O_Verbose: $oldverUp
-		Verbose:   $verUp
+  $BBok && {
+    test_connection
+    [ $? -ne 0 ] && exit
+    verUp=none; oldverUp=none; logUp=none; oldlogUp=none;
+    echo "Uploading logs"
+    [ -s $VERLOG ] && verUp=$(cat $VERLOG | nc termbin.com 9999)
+    [ -s $oldVERLOG ] && oldverUp=$(cat $oldVERLOG | nc termbin.com 9999)
+    [ -s $LOG ] && logUp=$(cat $LOG | nc termbin.com 9999)
+    [ -s $oldLOG ] && oldlogUp=$(cat $oldLOG | nc termbin.com 9999)
+    echo -n "Link: "
+    echo "$MODEL ($DEVICE) API $API\n$ROM\n$ID\n
+    O_Verbose: $oldverUp
+    Verbose:   $verUp
 
-		O_Log: $oldlogUp
-		Log:   $logUp" | nc termbin.com 9999
-	} || echo "Busybox not found!"
-	exit
+    O_Log: $oldlogUp
+    Log:   $logUp" | nc termbin.com 9999
+  } || echo "Busybox not found!"
+  exit
 }
 
 # Heading
 mod_head() {
-	clear
-	echo "$div"
-	echo "${W}$MODTITLE $VER${N}(${Bl}$REL${N})"
-	echo "by ${W}$AUTHOR${N}"
-	echo "$div"
-	echo "${W}$_bbname${N}"
-	echo "${Bl}$_bb${N}"
-	echo "$div"
-	[ -s $LOG ] && echo "Enter ${W}logs${N} to upload logs" && echo $div
+  clear
+  echo "$div"
+  echo "${W}$MODTITLE $VER${N}(${Bl}$REL${N})"
+  echo "by ${W}$AUTHOR${N}"
+  echo "$div"
+  echo "${W}$_bbname${N}"
+  echo "${Bl}$_bb${N}"
+  echo "$div"
+  [ -s $LOG ] && echo "Enter ${W}logs${N} to upload logs" && echo $div
 }
