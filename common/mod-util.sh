93b06c0d13483a70872fd04d781359001ec5b105
Val Mandantes <mandantesv@gmail.com>
2018-11-02 17:44:02 +0800

    Update mod-util.sh
    Signed-off-by: Val Mandantes <mandantesv@gmail.com>

--- common/mod-util.sh
+++ common/mod-util.sh
@@ -5,8 +5,8 @@
 #
 ##########################################################################################
 
-MODUTILVER=v2
-MODUTILVCODE=2
+MODUTILVER=v2.1
+MODUTILVCODE=21
 
 #=========================== Determine if A/B OTA device
 if [ -d /system_root ]; then
@@ -142,17 +142,23 @@ set_file_prop() {
 # https://github.com/fearside/ProgressBar
 # ProgressBar <progress> <total>
 ProgressBar() {
+# Determine Screen Size
+  if [[ "$COLUMNS" -le "53" ]]; then
+    local var1=2
+	local var2=20
+  else
+    local var1=4
+    local var2=40
+  fi
 # Process data
-  _progress=$(((${1}*100/${2}*100)/100))
-  _done=$(((${_progress}*4)/10))
-  _left=$((40-$_done))
+  local _progress=$(((${1}*100/${2}*100)/100))
+  local _done=$(((${_progress}*${var1})/10))
+  local _left=$((${var2}-$_done))
 # Build progressbar string lengths
-  _done=$(printf "%${_done}s")
-  _left=$(printf "%${_left}s")
+  local _done=$(printf "%${_done}s")
+  local _left=$(printf "%${_left}s")
 
-# 1.2 Build progressbar strings and print the ProgressBar line
-# 1.2.1 Output example:
-# 1.2.1.1 Progress : [########################################] 100%
+# Build progressbar strings and print the ProgressBar line
 printf "\rProgress : ${BGBL}|${N}${_done// /${BGBL}$loadBar${N}}${_left// / }${BGBL}|${N} ${_progress}%%"
 }
 
