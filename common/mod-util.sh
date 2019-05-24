1b35b7eba96ffe18e2b0d22d512bc00376cf1b16
Val Mandantes <mandantesv@gmail.com>
2019-05-24 21:27:45 +0800

    v17.2.1
    Signed-off-by: Val Mandantes <mandantesv@gmail.com>

--- common/mod-util.sh
+++ common/mod-util.sh
@@ -35,7 +35,7 @@ set_busybox() {
   if [ -x "$1" ]; then
     for i in $(${1} --list); do
       if [ "$i" != 'echo' ]; then
-        alias "$i"="${1} $i" >>$LOG 2>&1
+        alias "$i"="${1} $i" >/dev/null 2>&1
       fi
     done
     _busybox=true
